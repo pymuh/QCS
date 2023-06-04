dbfee9b60c280366988c74d94c3d6ad86f653c34
محمداسماعیل حسنی <35177091+pymuh@users.noreply.github.com>
2023-06-04 17:06:52 +0330

    Update example-shor.py

--- example-shor.py
+++ example-shor.py
@@ -5,12 +5,14 @@ n = 15
 O1 = Hadamard(4) * Identity(4)
 a = 7
 L = 2 ** 4
-preO2 = array([QDit((('|' + str((a**j)%n) + '>') , 4)).value.transpose()[0]
-               for j in range(L)]).transpose()
+
+preO2 = [[1 if j==pow(a, i, n) else 0 for i in range(L)] for j in range(L)]
 O2 = Identity(4) * operator(preO2)
+
 phi0 = QDit(('|0>', 8))
 phi1 = O1(phi0)
 phi2 = O2(phi1)
 phi3 = QFT(8)(phi2)
+
 obs = observer()
 obs.plot(phi3)
